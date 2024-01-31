ea9d0281f2539f852b65f2ba69e5544c028c453b
teli203@aol.com <your.email@example.com>
2024-01-30 23:06:40 -0600

    add getElem by id

---
+++ script.js
@@ -0,0 +1,6 @@
+const questionContainer = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+const restartButton = document.getElementById("restart-btn");
+const resultDiv = document.getElementById("result");
