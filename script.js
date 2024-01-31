7d3fa3eccbe3778727e253b0c8da4d7f4772b2fb
teli203@aol.com <your.email@example.com>
2024-01-30 23:08:09 -0600

    add shuffle

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 const restartButton = document.getElementById("restart-btn");
 const resultDiv = document.getElementById("result");
+
+let shuffledQuestions, currentQuestionIndex, score;
+
+const questions = [
+  {
+    question: "What is 2 + 2?",
+    answers: [
+      { text: "4", correct: true },
+      { text: "22", correct: false },
+      { text: "222", correct: false },
+      { text: "2222", correct: false },
+    ],
+  },
+  {
+    
+  }
