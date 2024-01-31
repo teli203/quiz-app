bb47a50c88c118105b53c040bb703022eee822ed
teli203@aol.com <your.email@example.com>
2024-01-31 11:55:14 -0600

    add cloud based questions

--- script.js
+++ script.js
@@ -9,39 +9,75 @@ let shuffledQuestions, currentQuestionIndex, score;
 
 const questions = [
   {
-    question: "What is 2 + 2?",
+    question: "What is the primary programming language supported by Amazon Web Services (AWS)?",
     answers: [
-      { text: "4", correct: true },
-      { text: "22", correct: false },
-      { text: "222", correct: false },
-      { text: "2222", correct: false },
+      { text: "Java", correct: true },
+      { text: "Ruby", correct: false },
+      { text: "Python", correct: false },
+      { text: "PHP", correct: false },
     ],
   },
   {
-    question: "What does HTML stand for?",
+    question: "What cloud provider is known for its serverless computing service called Azure?",
     answers: [
-      { text: "HyperText Markup Language", correct: true },
-      { text: "HighText Machine Language", correct: false },
-      { text: "HyperText Machine Language", correct: false },
-      { text: "HighText Markup Language", correct: false },
+      { text: "Microsoft", correct: true },
+      { text: "AWS", correct: false },
+      { text: "IBM", correct: false },
+      { text: "GCP", correct: false },
     ],
   },
   {
-    question: "Which property is used to change the background color?",
+    question: "What container orchestration service is provided bt Google Cloud platform?",
     answers: [
-      { text: "color", correct: false },
-      { text: "bgColor", correct: false },
-      { text: "background-color", correct: true },
-      { text: "background", correct: false },
+      { text: "Azure Kubernetes Service (AKS)", correct: false },
+      { text: "Amazon Elastic Kubernetes Service", correct: false },
+      { text: "Google Kubernetes Engine (GKE)", correct: true },
+      { text: "IBM Kubernetes Service", correct: false },
     ],
   },
   {
-    question: "Inside which HTML element do we put the JavaScript?",
+    question: "Which cloud provider is associated with the slogan 'The Intelligent Cloud'?",
     answers: [
-      { text: "<js>", correct: false },
-      { text: "<javascript>", correct: false },
-      { text: "<script>", correct: true },
-      { text: "<scripting>", correct: false },
+      { text: "Oracle", correct: false },
+      { text: "AWS", correct: false },
+      { text: "Microsoft Azure", correct: true },
+      { text: "Alibaba", correct: false },
+    ],
+  },
+  {
+    question: "What is the name of the object storage offered by AWS?",
+    answers: [
+      { text: "Amazon S3", correct: true },
+      { text: "Cloud Data Buckets", correct: false },
+      { text: "GKE Object Containers", correct: false },
+      { text: "Google Java Storage", correct: false },
+    ],
+  },
+  {
+    question: "Which cloud provider is known for its area of expertise in AI and Machine services?",
+    answers: [
+      { text: "Azure", correct: false },
+      { text: "Ruby", correct: false },
+      { text: "IBM", correct: false },
+      { text: "AWS", correct: true },
+    ],
+  },
+  {
+    question: "What networking service is provided by Google Cloud Platform for connecting on-premises networks to Google infrastructure?",
+    answers: [
+      { text: "Google Public Cloud", correct: false },
+      { text: "Google Virtual Private Cloud", correct: true },
+      { text: "Google Infrastructer RoBot", correct: false },
+      { text: "Google Search", correct: false },
+    ],
+  },
+  {
+    question: "Which cloud provider is associated with the term 'Elastic Load Balancer'?",
+    answers: [
+      { text: "IBM", correct: false },
+      { text: "Google", correct: false },
+      { text: "AWS", correct: true },
+      { text: "PHP", correct: false },
     ],
   },
 ];
